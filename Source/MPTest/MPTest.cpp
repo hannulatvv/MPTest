d416403b32f1001f555cc54eb552e777c8cf6853
Teemu Hannula <hannula.tvv@gmail.com>
2022-08-02 21:00:25 +0300

    created project

---
+++ Source/MPTest/MPTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MPTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MPTest, "MPTest" );
---
+++ Source/MPTest/MPTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MPTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MPTEST_API AMPTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
